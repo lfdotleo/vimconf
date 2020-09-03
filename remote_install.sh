3da9e7cc145238ba7f69cd6256a8159874cc9539
liufei <liufei@mkit.com.cn>
2020-09-03 19:26:17 +0800

    update

--- remote_install.sh
+++ remote_install.sh
@@ -11,5 +11,4 @@ echo "let g:airline#extensions#tabline#enabled=1" >> ~/.vimrc
 echo "let g:airline#extensions#tabline#buffer_nr_show=1" >> ~/.vimrc
 echo "map <leader>ud :UndotreeToggle<CR>" >> ~/.vimrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
