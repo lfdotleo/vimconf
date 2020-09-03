d57dd348e5bcf1f1536166c581bbc30283927bdd
liufei <liufei@mkit.com.cn>
2020-09-03 19:14:50 +0800

    add remote_install.sh

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## Use
 
 `git clone  https://github.com/codekeeperjava/vimconf.git` & `./install.sh`
+
+## Dependence
+
+1. tagbar depends on ctags
+2. smartim depends on im-select
+
+## Optition
+
+1. `ale` It is too big, if you don’t need to check the syntax, you don’t need to install it.
+2. `smartim` On the remote server, you don’t need to install it.
---
+++ remote_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cat ./base.vim > ~/.vimrc
+
+echo "call plug#begin('~/.vim/plugged')" >> ~/.vimrc
+echo "Plug 'vim-airline/vim-airline" >> ~/.vimrc
+echo "Plug 'mbbill/undotree'" >> ~/.vimrc
+echo "call plug#end()" >> ~/.vimrc
+echo "let g:airline#extensions#tabline#enabled=1" >> ~/.vimrc
+echo "let g:airline#extensions#tabline#buffer_nr_show=1" >> ~/.vimrc
+echo "map <leader>ud :UndotreeToggle<CR>" >> ~/.vimrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
