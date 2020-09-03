9722483e277966b995f23ace090a7a838a7b161c
liufei <liufei@mkit.com.cn>
2020-09-03 19:20:13 +0800

    add remote_install.sh

--- remote_install.sh
+++ remote_install.sh
@@ -5,6 +5,7 @@ cat ./base.vim > ~/.vimrc
 echo "call plug#begin('~/.vim/plugged')" >> ~/.vimrc
 echo "Plug 'vim-airline/vim-airline" >> ~/.vimrc
 echo "Plug 'mbbill/undotree'" >> ~/.vimrc
+echo "Plug 'itchyny/vim-cursorword'" >> ~/.vimrc
 echo "call plug#end()" >> ~/.vimrc
 echo "let g:airline#extensions#tabline#enabled=1" >> ~/.vimrc
 echo "let g:airline#extensions#tabline#buffer_nr_show=1" >> ~/.vimrc
